a066d95ee9b87046ebd76bec2a94e3b0a611c844
lohith2320 <140438967+lohith2320@users.noreply.github.com>
2024-02-07 18:11:40 +0530

    Add files via upload

---
+++ dayfour/Base.java
@@ -0,0 +1,38 @@
+package dayfour;
+
+public class Base {
+
+		int varDefault = 10;
+		public int varPublic = 20;
+		private int varprivate = 30;
+		protected int varProtected = 40;
+		
+		void methodDefault()
+		{
+			System.out.println("Default variable " +varDefault);
+			System.out.println("Deafult method");
+		}
+		public void methodPublic()
+		{
+			System.out.println("Public variable " +varPublic);
+			System.out.println("Public method");
+		}
+		private void methodPrivate()
+		{
+			System.out.println("Private variable " +varprivate);
+			System.out.println("Private method");
+		}
+
+		
+		protected void methodProtected()
+		{
+			System.out.println("Protected variable " +varProtected);
+			System.out.println("Protected method");
+		}
+	
+		
+
+	}
+
+
+
---
+++ dayfour/Executor.java
@@ -0,0 +1,18 @@
+package dayfour;
+
+public class Executor {
+	public static void main(String[] args) {
+
+
+		Base obj = new Base();
+		
+		obj.methodDefault();
+		obj.methodProtected();
+		obj.methodPublic();
+	
+		
+	}
+
+}
+
+
