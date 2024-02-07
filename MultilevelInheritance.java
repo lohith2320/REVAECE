52cd42deb2ef521e3563ae9f26edebc74c4196f8
lohith2320 <140438967+lohith2320@users.noreply.github.com>
2024-02-07 22:46:09 +0530

    Add files via upload

---
+++ MultilevelInheritance.java
@@ -0,0 +1,19 @@
+package day5;
+
+public class MultilevelInheritance {
+	
+	public static void main(String[] args) {
+		city city = new city();
+		city.setCityName("Bangalore");
+		city.setArea(264.2f);
+		city.setStateName("Karnataka");
+		city.setLanguage("Kannada");
+		city.setCountryName("India");
+		city.setCapital("Delhi");
+		System.out.println(city);
+
+	}
+
+}
+
+
---
+++ country.java
@@ -0,0 +1,24 @@
+package day5;
+
+public class country {
+
+	private String countryName;
+	private String capital;
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	@Override
+	public String toString() {
+		return "country [countryName=" + countryName + ", capital=" + capital + "]";
+	}
+	
+}
